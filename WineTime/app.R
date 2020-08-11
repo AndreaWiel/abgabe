c811219048ce16c37769f375b2a0c1476e131e09
AndreaWiel <andrea.wiel@rwth-aachen.de>
2020-08-11 16:13:57 +0200

    Umlaute entfernt

--- WineTime/app.R
+++ WineTime/app.R
@@ -14,6 +14,18 @@ library(bootstrap)
 library(leaflet)
 library(ggplot2)
 library(plotly)
+library(ggthemes)
+library(gghighlight)
+library(lubridate)
+library(tidyverse)
+library(shinydashboard)
+
+# Daten laden
+
+raw <- read_csv("https://github.com/AndreaWiel/abgabe/blob/master/WineTime/csv_Datens%C3%A4tze/Wein/Erntemenge_Bundesl%C3%A4nder_Jahr_Rebsorte.csv")
+raw <- read_csv("https://github.com/AndreaWiel/abgabe/blob/master/WineTime/csv_Datens%C3%A4tze/Wein/Rebfl%C3%A4schen_Anbaugebiete_Jahr_Rebsorte.csv")
+raw <- read_csv("https://github.com/AndreaWiel/abgabe/blob/master/WineTime/csv_Datens%C3%A4tze/Wein/Erntemenge_Bundesl%C3%A4nder_Jahr_Rebsorte.csv")
+raw <- read_csv("https://github.com/AndreaWiel/abgabe/blob/master/WineTime/csv_Datens%C3%A4tze/Wein/Erntemenge_Bundesl%C3%A4nder_Jahr_Rebsorte.csv")
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(

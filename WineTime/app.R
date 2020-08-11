4265fda638f00841cc756415ff79ec0477c5ea53
Susanna Formanns <susanna.formanns@rwth-aachen.de>
2020-08-11 14:54:19 +0200

    Änderungen

---
+++ WineTime/app.R
@@ -0,0 +1,54 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinythemes)
+library(shinyjs)
+library(bootstrap)
+library(leaflet)
+library(ggplot2)
+library(plotly)
+
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+        title ="WineTime",
+        theme = "bootstrap.min.css",
+        footer = includeHTML("footer.html"),
+        fluid = TRUE, 
+        collapsible = TRUE,
+
+        # tabPanel 1 - Home
+        tabPanel("Home"
+        ),
+        
+        # tabPanel 2 - Weinanbaugebiete
+        tabPanel("Weinanbaugebiete"
+        ),
+        
+        # tabPanel 3 - Ernte
+        tabPanel("Ernte"
+        ),
+        
+        # tabPanel 4 - Weinproduktion
+        tabPanel("Weinproduktion"
+        ),
+        
+        # tabPanel 5 - Weinbestände
+        tabPanel("Weinbestände"
+        )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
